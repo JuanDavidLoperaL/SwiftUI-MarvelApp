c5a7075d28723eca10c448d789b0a32c49a2aab6
Juan Lopez <juanl@backbase.com>
2023-10-03 13:53:34 -0500

    Init project in Swift UI
    Init project in Swift UI

---
+++ MarvelApp/MarvelApp/MarvelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAppApp.swift
+//  MarvelApp
+//
+//  Created by Juan david Lopera lopez on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
